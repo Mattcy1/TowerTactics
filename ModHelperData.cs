cbebb4c6a1c0e61ff9e8a2ca51a4f3b5fb825cc9
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-06-04 12:30:05 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,27 @@
+﻿namespace TowerTactics;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "43.0";
+    public const string Version = "1.0.0";
+    public const string icon = "Icon";
+    public const string Name = "Tower Tactics";
+
+    public const string Description = "Introducing Tower Tactics! Experience a new strategic dimension in 'Bloons TD 6' with our revolutionary tower maintenance and fatigue system! Prepare to push the limits of your defense skills with unprecedented challenges and advanced tactical management.\n\n" +
+        "Fatigue Bar: Push Your Towers to the Limit\n" +
+        "Every tower is now equipped with a fatigue bar that fills up over time as it relentlessly fires at the Bloons. The more your towers work, the more they wear out. If the fatigue bar reaches its maximum, your towers risk becoming less effective or even stopping temporarily. It is crucial to monitor this bar and adopt a rotation and rest strategy to keep your defense line operational.\n\n" +
+        "Ammunition: Fuel Your Arsenal\n" +
+        "Times have changed, and your towers now need ammunition to function. All towers use the same type of ammunition: Darts. Earn money by popping Bloons and use it to purchase ammo recharges. Smart budget and ammo management are essential to ensure you never run out of firepower.\n\n" +
+        "Periodic Taxes: Keep Your Towers Operational\n" +
+        "In addition to managing fatigue and ammunition, you'll need to pay taxes every 10 rounds to keep your towers operational. If you refuse to pay, all your towers will be sold. If you accept, your towers will remain, but your cash will be halved by 30%. This adds an extra layer of strategy, as you'll need to balance your spending on upgrades, ammunition, and periodic taxes to ensure your towers remain active.\n\n" +
+        "User Interface (UI): Simplified Management\n" +
+        "Our new user interface offers simplified and intuitive management of your towers. Easily track fatigue bars, ammo levels, and periodic payment timers for each tower. Use dedicated buttons to perform manual pauses and repairs in real-time.\n\n" +
+        "Prepare for an Unmatched Gaming Experience\n" +
+        "With the tower maintenance and fatigue system, every decision counts. Dive into a deeper and more strategic gameplay experience where smart resource management and anticipating your towers' needs can make the difference between victory and defeat. Adaptability, strategy, and mastery are the keys to triumphing over the waves of Bloons with these new mechanics.\n\n" +
+        "Are you ready to take on the challenge? Enter the arena and showcase your ultimate defense skills in 'Bloons TD 6: Tower Tactics'!";
+
+
+    public const string RepoOwner = "Mattcy"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "TowersTactics"; // TODO add your repo name here, also in the download url in README.md
+}
+
---
+++ README.md
@@ -0,0 +1,39 @@
+<a href="https://github.com/Mattcy1/TowerTactics/releases/latest/download/TowerRevamp.dll">
+    <img align="left" alt="Icon" height="90" src="Icon.png">
+    <img align="right" alt="Download" height="75" src="https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/BloonsTD6%20Mod%20Helper/Resources/DownloadBtn.png">
+</a>
+
+<h1 align="center">Introducing Tower Tactics!</h1>
+
+### What is Tower Tactics?
+
+Tower Tactics is a mod that revamps the entire tower system with three mechanics:
+
+- * Fatigue
+- * Taxes
+- * Ammo
+
+### How does Fatigue Work?
+
+Every time a tower shoots, there is a 1/15 chance it will become more fatigued.
+- At 30 fatigue, the tower takes an extra 5 seconds to shoot.
+- You can reset its fatigue and rate by feeding it bananas.
+- You can buy bananas from the shop.
+- Fatigue works per tower type, so if one Dart Monkey gets fatigued, all Dart Monkeys will also be fatigued.
+
+### How do Taxes Work?
+
+Every 10 rounds, you will need to pay the tower's taxes.
+- If you don't pay, they will quit the war.
+- If you do pay, they will stay with you at the cost of 30% of your money.
+
+### How does Ammo Work?
+
+Every time a tower shoots, its ammo decreases by 1.
+- When you run out of ammo, your tower will become useless.
+- You can buy ammo from the shop!
+
+<h1 aling="left"><img alt="GitHub all releases" height="25" src="https://img.shields.io/github/downloads/Mattcy1/TowerTactics/total?label=Total%20Dowloads"></h1>
+
+
+[![Requires BTD6 Mod Helper](https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/banner.png)](https://github.com/gurrenm3/BTD-Mod-Helper#readme)
